49a0b773615efa2947202644b003edb28ff3054f
Micah Stetson <micah@schoolsplp.com>
2024-02-12 21:15:31 -0800

    Support bearer tokens with no prefix

--- cmd/api/auth_bearer.go
+++ cmd/api/auth_bearer.go
@@ -8,8 +8,9 @@ import (
 )
 
 type BearerAuthConfig struct {
-	Prefix string // Leave blank for "Bearer"
-	Token  string
+	Prefix   string // Leave blank for "Bearer"
+	Token    string
+	NoPrefix bool
 }
 
 func newBearerAuthClient(c *Config, a *apiconfig.AuthState) (Client, error) {
@@ -19,12 +20,15 @@ func newBearerAuthClient(c *Config, a *apiconfig.AuthState) (Client, error) {
 	if c.BearerAuth.Prefix == "" {
 		c.BearerAuth.Prefix = "Bearer"
 	}
+	client := bearerAuthClient{Client: http.DefaultClient}
 	var deref apiconfig.Dereffer
-	return bearerAuthClient{
-		Client: http.DefaultClient,
-		Authorization: deref.String(c.BearerAuth.Prefix) +
-			" " + deref.String(c.BearerAuth.Token),
-	}, deref.Error
+	if c.BearerAuth.NoPrefix {
+		client.Authorization = deref.String(c.BearerAuth.Token)
+	} else {
+		client.Authorization = deref.String(c.BearerAuth.Prefix) +
+			" " + deref.String(c.BearerAuth.Token)
+	}
+	return client, deref.Error
 }
 
 type bearerAuthClient struct {
